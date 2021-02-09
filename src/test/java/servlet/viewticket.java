4c9501a71ea5abe661bb0eb30bd998688e7f572e
samy <vrsamy2k@gmail.com>
2021-02-09 13:27:25 -0500

    Test server IP updated

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://54.218.59.188:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://54.218.76.179:5432/postgres";
 			String username="postgres";
 			String password="password";
 						

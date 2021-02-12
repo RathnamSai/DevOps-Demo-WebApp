5e7212c8442f64741b7acf51975b04de50b65632
samy <vrsamy2k@gmail.com>
2021-02-12 14:30:31 -0500

    Ip address updated

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://54.218.58.176:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://34.221.228.165:5432/postgres";
 					String username="postgres";
 					String password="password";
 					

5e109f536e2af1d6f5589d73f66d2efe5a217d4e
samy <vrsamy2k@gmail.com>
2021-03-08 13:43:58 -0500

    IP address updated

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -67,6 +67,56 @@ public class acat {
 		//		.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Repair Ticket Page']")));
 		//repair1.isDisplayed();
 	}
+	
+	@Test(priority = 2)
+	public void navigation() throws Exception {
+	//	WebElement welcome = wait.until(ExpectedConditions
+		//		.visibilityOfElementLocated(By.xpath("//h2[text()='WELCOME TO YOUR ACCOUNT OVERVIEW']")));
+		//welcome.isDisplayed();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//WebElement prepair = wait.until(ExpectedConditions
+		//		.visibilityOfElementLocated(By.xpath("//p[text()='REPAIR']")));
+		//prepair.isDisplayed();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(URL1);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		WebElement repair = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath("//font[text()='Create Repair Ticket']")));
+		repair.isDisplayed();
+		WebElement cancel = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Cancel']")));
+		cancel.isDisplayed();
+		//cancel.click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		Thread.sleep(3000);
+		//WebElement repair1 = wait
+		//		.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Repair Ticket Page']")));
+		//repair1.isDisplayed();
+	}
+	
+	@Test(priority = 3)
+	public void checkout() throws Exception {
+	//	WebElement welcome = wait.until(ExpectedConditions
+		//		.visibilityOfElementLocated(By.xpath("//h2[text()='WELCOME TO YOUR ACCOUNT OVERVIEW']")));
+		//welcome.isDisplayed();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//WebElement prepair = wait.until(ExpectedConditions
+		//		.visibilityOfElementLocated(By.xpath("//p[text()='REPAIR']")));
+		//prepair.isDisplayed();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(URL1);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		WebElement repair = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath("//font[text()='Create Repair Ticket']")));
+		repair.isDisplayed();
+		WebElement cancel = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Cancel']")));
+		cancel.isDisplayed();
+		//cancel.click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		Thread.sleep(3000);
+		//WebElement repair1 = wait
+		//		.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Repair Ticket Page']")));
+		//repair1.isDisplayed();
+	}
 
 	
 	/*@Test(priority = 2)
